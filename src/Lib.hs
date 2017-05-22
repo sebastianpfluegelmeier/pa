6704073188f021de5d5431c1d56a2c22104f7738
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-05-22 22:28:10 +0200

    uniOsc works

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,16 +6,36 @@ module Lib
 import Csound.Base
 import Data.List (intersperse)
 
+
+-- TODO: solve :
 uniOsc :: (Sig -> Sig) -> Sig -> Int -> Sig -> SE (Sig, Sig)
 
-uniOsc iosc detune voices freq = 
-    (\x y -> (x, y)) <$> side <*> side
+uniOsc iosc detune voicesNum freq = 
+    fmap spread (sequence voices)
       where
-        side :: SE Sig
-        side = foldr1 (+) (replicate voices voice)
+        voices :: [SE Sig]
+        voices = replicate voicesNum voice
 
         voice :: SE Sig
-        voice = fmap (\x -> iosc $ freq + expseg [10000, 0.001, 1] * detune * lp 100 1 x) pink
+        voice = fmap (\x -> 
+                        iosc $ freq + 
+                               detune * lp (linseg [1000, 0.0001, 0.4]) 0.2 x) pink
+
+        spread :: [Sig] -> (Sig, Sig)
+        spread x = 
+            foldr1 (\(x1, x2) (y1, y2) -> (x1 + y1, x2 + y2)) $ zipped x
+
+        zipped :: [Sig] -> [(Sig, Sig)]
+        zipped x = 
+            zipWith 
+                pan
+                x 
+                [0.0, (0.0 + 1.0 / (fromIntegral $ length x)) .. 1]
+
+        pan :: Sig -> Double -> (Sig, Sig)
+        pan signal lr = (signal * (sig $ double $ lr), signal * (1.0 - (sig $ double lr)))
+
+            
 
 stereoWidth :: Sig -> (Sig, Sig) -> (Sig, Sig)
 stereoWidth amt (left, right) =
