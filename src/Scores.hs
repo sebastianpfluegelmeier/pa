8b893d28e227deb4b7aac5421054cf728eb2dc9c
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-05-25 20:09:06 +0200

    failed attempt to makeing a cool beat

--- src/Scores.hs
+++ src/Scores.hs
@@ -1,22 +1,34 @@
 module Scores 
 ( score1
+, groove1
+, score2
+, groove2
 )
 where
 import Csound.Base
 import Lib
 
-score1 :: Sco D 
-score1 = sco $ fmap ((220 *).double) [ 7.0/4.0
-                                     , 6.0/5.0
-                                     , 1.0.0.0
-                                     , 4.0/3.0
-                                     , 3.0/2.0
-                                     , 5.0/3.0
-                                     , 6.0/4.0
-                                     ]
+toScore :: [Double] -> Sco D 
+toScore list = mel $ fmap (temp.double) list
+
+list1 = [1.0,2.0,1.0,2.0,1.0]
+list2 = [2.0,3/2,4/3,5/6,5/3]
+list3 = [9/5,8/4,7/3,6/2,8/5]
+
+score1 = toScore $ fmap (* 125) $ list1 ++ list1 ++ list2 ++ list3
 
 groove1 = 
-    str 0.3 g1
+    str 0.3 $ loopBy 4 g1
       where 
-        g1 = groove [3, 3, -2, 2,  1, 3, 3]
+        g1 = groove [1, 1, -2, 1,  -1, 2, 8]
+
+groove2 = 
+    str 0.3 $ loopBy 4 g1
+      where
+        g1 = groove [2, 2, -1, 3, 3, 5]
+
+score2 = toScore $ fmap (* (125/2)) $ list1 ++ list4 ++ list1 ++ list5
+
+list4 = [1.0, 6/2, 2.0, 1.0, 6/4]
+list5 = [1.0, 10/4, 6/2, 2.0, 9/5]
 
--- src/Tracks.hs
+++ src/Tracks.hs
@@ -7,4 +7,6 @@ import Lib
 import Scores
 import Patches
 
-track1 = mix $ sco bass1 score1
+track1 :: (Sig, Sig)
+track1 = mix $ har [sco phatFM (applyGroove (str 0.4 groove1) score1),
+                    sco bass1 (applyGroove (str 0.4 groove2) score2)]
