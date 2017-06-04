5e2ff942b1066bd068b53edaf327a16303c2f25f
sebastian pfluegelmeier <pfluegelmeier.s@gmx.at>
2017-06-04 12:09:18 +0200

    made some operators to chain functions with different types (Sig, Sig2, SE Sig, SE Sig2)

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,120 @@
 module Lib
-    ( uniOsc
+    ( (>>>)
+    , (==>)
+    , (<==)
+    , (>->)
+    , (<->)
+    , (<-<)
+    , (>>-)
+    , (-<<)
+    , sFmap
+    , sFmap2 
+    , sFmap3 
+    , sFmap4 
+    , collectTuple
+    , collectTupleA
+    , toStereo
+    , toStereoFX
+    , liftToStereo
+    , liftToStereoFX
+    , uniOsc
     , stereoWidth
-    , liftToSESigs
     , applyGroove
     , groove
+    , loadSam
+    , loadSam1
     ) where
 import Csound.Base
+import Csound.Sam
 import Data.List (intersperse)
 
+-- ################  conversion operators ################
+
+{- conversion operator table
+ -
+ -  X >-= Y
+ -
+ -                     ************ X ******************
+ -                     | Sig | Sig2 | SE Sig | SE Sig2 |
+ -  *  Sig  ->    Sig  | >>> | ==>  |  <->   |   >->   |
+ -  Y  Sig2 ->    Sig2 | <== | >>>  |  <-<   |   <->   |
+ -  *  Sig  -> SE Sig  | >>> | ==>  |  >>=   |   >>-   |
+ -  *  Sig2 -> SE Sig2 | <== | >>>  |  -<<   |   >>=   |
+ -}
+
+sFmap :: (Functor f) => (a -> b) -> f (a, a) -> f (b, b)
+sFmap fn val = fmap (\(l, r) -> (fn l, fn r)) val
+
+sFmap2 :: (Applicative f) => (a -> b -> c) -> f (a, a) -> f (b, b) -> f (c, c)
+sFmap2 fn val1 val2 = (\(l1, r1) (l2, r2) -> (fn l1 l2, fn r1 r2)) <$> val1 <*> val2
+
+sFmap3 :: (Applicative f) => (a -> b -> c -> d) -> 
+                             f (a, a) ->
+                             f (b, b) ->
+                             f (c, c) ->
+                             f (d, d)
+sFmap3 fn val1 val2 val3 = (\(l1, r1) (l2, r2) (l3, r3) -> 
+                               (fn l1 l2 l3 , fn r1 r2 r3)
+                           ) 
+                           <$> val1 <*> val2 <*> val3
+
+sFmap4 :: (Applicative f) => (a -> b -> c -> d -> e) -> 
+                             f (a, a) ->
+                             f (b, b) ->
+                             f (c, c) ->
+                             f (d, d) ->
+                             f (e, e)
+sFmap4 fn val1 val2 val3 val4 = (\(l1, r1) (l2, r2) (l3, r3) (l4, r4) -> 
+                                     (fn l1 l2 l3 l4, fn r1 r2 r3 r4)
+                                 ) 
+                                 <$> val1 <*> val2 <*> val3 <*> val4
+
+
+(>>>) :: a -> (a -> b) -> b
+val >>> fn = fn val
+
+(==>) :: (a, a) -> (a -> a) -> (a, a) 
+(valL, valR) ==> fn = (fn valL, fn valR)
+
+(<==) :: a -> ((a, a) -> (b, b)) -> (b, b)
+val <== fn = fn (val, val)
+
+(>->) :: (Functor f) => f (a, a) -> (a -> b) -> f (b, b)
+val >-> fn = fmap (\(l, r) -> (fn l, fn r)) val
+
+(<-<) :: (Functor f) => (f a) -> ((a, a) -> (b, b)) -> f (b, b)
+val <-< fn = fmap (\x -> fn (x, x)) val
+
+(<->) :: (Functor f) => f a -> (a -> b) -> f b
+val <-> fn = fmap fn val
+
+(>>-) :: (Monad m) => m (a, a) -> (a -> m b) -> m (b, b)
+val >>- fn = val >>= (\(x, y) -> fmap (\(a:b:_) -> (a, b)) $ sequence [fn x, fn y])
+
+(-<<) :: (Monad m) => m a -> ((a, a) -> m (b, b)) -> m (b, b)
+val -<< fn = val >>= (\x -> fn (x, x))
+
+-- ################  conversion functions  ################
+
+collectTuple :: (Monad m) => (m a, m a) -> m (a, a)
+collectTuple (x, y) = fmap (\(a:b:_) -> (a, b)) $ sequence [x, y]
+
+collectTupleA :: (Applicative m) => (m a, m a) -> m (a, a)
+collectTupleA (x, y) = fmap (\(a:b:_) -> (a, b)) $ sequenceA [x, y]
+
+toStereo :: a -> (a, a)
+toStereo x = (x, x)
+
+liftToStereo :: (Applicative f) => f a -> f (a, a)
+liftToStereo x = collectTupleA (x, x)
+
+toStereoFX :: (a -> b) -> a -> (b, b)
+toStereoFX fn val = (fn val, fn val)
+
+liftToStereoFX :: (Applicative f) => (a -> b) -> f a -> f (b, b)
+liftToStereoFX fn val = collectTupleA (fmap fn val, fmap fn val)
+
+-- ################    audio functions     ################
 
 -- TODO: solve : phase problem
 uniOsc :: (Sig -> Sig) -> Sig -> Int -> Sig -> SE (Sig, Sig)
@@ -40,16 +147,13 @@ uniOsc iosc detune voicesNum freq =
         pan :: Sig -> Double -> (Sig, Sig)
         pan signal lr = (signal * (sig $ double $ lr), signal * (1.0 - (sig $ double lr)))
 
-            
-
 stereoWidth :: Sig -> (Sig, Sig) -> (Sig, Sig)
 stereoWidth amt (left, right) =
     (amt * left + mid * (1 - amt), amt * right + mid * (1 - amt))
       where
         mid = left + right
 
-liftToSESigs :: (Sig -> Sig) -> SE (Sig, Sig) -> SE (Sig, Sig)
-liftToSESigs fn = fmap (\(x, y) -> (fn x, fn y))
+-- ################  Score Functions  ################
 
 applyGroove :: Sco a -> Sco b -> Sco b
 applyGroove groove score =
@@ -73,3 +177,38 @@ groove nList =
                    str (double x) $ temp ()
                  else
                    rest (double (-x))
+
+{- a function to create scores easily
+ - you can write notes in the form "c d d# e"
+ - it creates a score with a whole note c, a whole note d, a whole note d# etc.
+ - you can also write notes in the form "#1/4 c d $1/2 d# e"
+ - it creaes a score with a quarter note c and d, then a half note d# and then a quarter note e"
+ - the #length sets the note length for all folowing notes
+ - the $length sets the length for one following note
+ - you can write a pause with "_" 
+ - you can set the octave with ''' or '3 and ,,, or ,3 and $,3 for a single note
+sc :: [Char] -> Sco D
+sc string = sc' 0
+  where
+    sc' :: Int -> Double -> [Char] -> Sco D
+    sc' _ _ [] = rest 0
+    sc' octave length string = 
+        |isNote $ firstToken string =
+            mix [note, sc' octave length ()]
+ -}
+
+
+-- ################  Sampler Fucntions  ################
+
+loadSam :: [Char] -> SE Sig2
+loadSam directory = 
+    runSam 1 $ wav samDir 
+      where 
+        samDir = "/home/sebastian/samples/" ++ directory
+
+loadSam1 :: [Char] -> SE Sig2
+loadSam1 directory = 
+    runSam 1 $ wav1 samDir 
+      where 
+        samDir = "/home/sebastian/samples/" ++ directory
+
--- src/Tracks.hs
+++ src/Tracks.hs
@@ -1,158 +1,16 @@
 module Tracks
 ( track1
-, bd
-, sn 
-, h1
-, h2
-, ho
-, p1
-, p2
-, p3
-, sh
-, cr
-, unGrooved1
-, groove1
-, groove2
-, grooved1
-, grooved2
-, grooved3
-, grooved4
-, shgroove
-, mixGroove1
-, padSynth
 )
 where 
 import Csound.Base
 import Csound.Sam   
 import Lib
 
-track1 :: SE Sig2
-track1 = return $ toStereo $ sig 0
+track1 :: IO ()
+track1 = (sFmap2 
+             blp 
+             (liftToStereo $ fmap (\x -> 500 + 400 * uosc (4 + 3 * blp 0.4 x )) white)
+             (liftToStereo $ return $ saw 220))
+         >-> (distortion 10) 
+         >>> dac 
 
--- ### Drum Samples ###
-
-sn :: D -> SE Sig2
-sn _ = loadSam "drums_and_single_hits/snares/metal-sd3.wav"
-
-bd :: D -> SE Sig2
-bd _ = 
-    (+) <$> fadeInOut 0.2 4 kick1 <*> fadeInOut 0.001 0.3 kick2
-      where
-        fadeInOut in_ out = (fmap (toStereoFX (\x -> 1 * x * fadeIn in_ * fadeOut out)))
-        kick1 = loadSam1 "drums_and_single_hits/kicks/giant-afro-kick1.wav"
-        kick2 = loadSam1 "drums_and_single_hits/kicks/gorgeous-kick.wav"
-
-h1 :: D -> SE Sig2
-h1 _ = fmap (toStereoFX (* 0.7)) $ loadSam1 "drums_and_single_hits/hats/clear-hat.wav"
-
-h2 :: D -> SE Sig2
-h2 _ = fmap (toStereoFX (* 0.5)) $ loadSam1 "drums_and_single_hits/hats/chh.wav"
-
-ho :: D -> SE Sig2
-ho _ = fmap (toStereoFX (* 0.7)) $ loadSam1 "drums_and_single_hits/hats/bigopenhh.wav"
-
-p1 :: D -> SE Sig2
-p1 _ = fmap (toStereoFX (* 0.7)) $ loadSam1 "drums_and_single_hits/tabla/tabla5.aif"
-
-p2 :: D -> SE Sig2
-p2 _ = fmap (toStereoFX (* 0.7)) $ loadSam1 "drums_and_single_hits/tabla/dhi.aif"
-
-p3 :: D -> SE Sig2
-p3 _ = fmap (toStereoFX (* 0.7)) $ loadSam1 "drums_and_single_hits/japanese/shime_hi_2.aif"
-
-sh :: D -> SE Sig2
-sh x = fmap (toStereoFX (* (sig x * 0.6))) $ loadSam1 
-           "drums_and_single_hits/percussion_western_and_latin/maraca.aif"
-
-cr :: D -> SE Sig2
-cr _ = fmap (toStereoFX (* 0.2)) $ loadSam1 
-           "drums_and_single_hits/crashes/muted_crash.aif"
-
-crash :: Sco (Mix (Sig2))
-crash = str 0.11 $ sco cr (temp 0)
-
-shgroove :: Sco (Mix (Sig2))
-shgroove = str 0.11 $ mel $ fmap (\x -> sco sh (temp x)) [1, 0.3,0.6, 0.4]
-
-groove1 :: Sco ()
-groove1 = str 0.11 $ groove [3, 1, 2, 1, 1, 2, 2, 1, 2, 1]
-groove2 = str 0.11 $ groove [1, 3, 2, 1, 1, 2, 2, 1, 2, 1]
-
-unGrooved1 = mel $ fmap (\x -> sco x $ temp $ double 1) $ [bd, h1, sn, ho, bd, p2, h2, sn, h1, p3]
-unGrooved2 = mel $ fmap (\x -> sco x $ temp $ double 1) $ [bd, ho, sn, h2, h1, h2, bd, sn, p3, h1]
-
-grooved1 = applyGroove groove1 unGrooved1
-grooved2 = applyGroove groove2 unGrooved1
-grooved3 = applyGroove groove1 unGrooved2
-grooved4 = applyGroove groove2 unGrooved2
-
-mixGroove1 = 
-    mix $ 
-        loopBy 2 $ 
-            har 
-                [ loopBy 32 shgroove
-                , loopBy 2  $ str 64 $ crash
-                , mel 
-                    [ grooved1
-                    , grooved2
-                    , grooved1
-                    , grooved3
-                    , grooved2
-                    , grooved3
-                    , grooved2
-                    , grooved4
-                    ]
-                ]
-
-padSynth :: D -> SE Sig2
-padSynth freq =
-    (\(xl, xr) (yl, yr) -> 
-        ((filter $ env1 * xl + env2 * yl), 
-         (filter $ env1 * yr + env2 * xr)
-        )
-    ) <$> 
-    uniOsc osc1 (sig 50) 3 (sig freq) <*> 
-    uniOsc osc2 (70 * env1) 3 (sig freq)
-      where
-        env1 :: Sig
-        env1 = fadeIn (0.1 + decay / 10) * fOut (decay * 1.4)
-        env2 :: Sig
-        env2 = fOut decay
-        osc1 :: Sig -> Sig
-        osc1 x = mlp ((4 + env1 * 2 * (0.5 + uosc 2 * 100) ) * x) 1.9 
-                   (osc (x + 10000 * sqr (4.1 * x))) + 
-               osc (x + 100 * env2 * osc (x * 4))
-        osc2 :: Sig -> Sig
-        osc2 x = oscBy (sines [1, 0.2, 1.5, 0.1, 1.8, 0.4, 0.2, 0.1, 0.1, 1.01]) x
-        filter :: Sig -> Sig
-        filter = lp (sig freq * 2 * (1 + 2 * env1 * uosc (1 + 8 * env2))) 1.4
-        fOut :: D -> Sig
-        fOut x = linseg [1, x, 0]
-        decay = 5
-
--- padScore :: Sco D
-
-{-
-padScore = fmap (fmap cpspch) $
-                            [ [ 3.00
-                              , 3.03
-                              , 3.10
-                              , 4.02
-                              ]
-                            , [ 3.05
-                              , 3.08
-                              , 3.12
-                              , 4.07
-                              ]
-                            , [ 2.10
-                              , 3.01
-                              , 3.05
-                              , 3.12
-                              ]
-                            , [ 3.05
-                              , 3.08
-                              , 3.12
-                              , 4.07
-                              ]
-                            ]
-                            -}
